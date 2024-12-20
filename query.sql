762fff6243876750cc2ae28c97b95a068af54032
JEEVITH <150571568+JEEVITH14@users.noreply.github.com>
2024-12-20 15:10:39 +0530

    1
    1

---
+++ query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `tbl_Cus` (
+    `Cus_name` varchar(255) NOT NULL,
+	`Cus_phone` int(11) NOT NULL,
+	`Cus_mail` varchar(255) NOT NULL,
+	`Cus_type` varchar(255) NOT NULL,
+	`Cus_des` varchar (255) NOT NULL 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
